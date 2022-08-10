fd296d8c11d6cabf37be946763e7e9127623e173
m-g-k <mgk@uk.ibm.com>
2022-08-10 23:15:46 +0100

    optimize windows server runs

--- build-and-ut.cmd
+++ build-and-ut.cmd
@@ -6,11 +6,15 @@ REM Licensed under the MIT license (see LICENSE for details)
 REM 
 
 REM Create the work directory
-set WORKDIR=%TEMP%\ace-demo-ut-work-dir6
+set WORKDIR=%TEMP%\ace-demo-ut-work-dir7
 call mqsicreateworkdir %WORKDIR%
 
 REM ibmint for v12
 ibmint deploy --input-path . --output-work-directory %WORKDIR% --project TeaAvailabilityChecker --project TeaAvailabilityChecker_Working_Test
+
+REM optimize server
+ibmint optimize server --work-directory %WORKDIR%
+
 :: mqsisetdbparms -w %WORKDIR% -n TEADB -u hzp00167 -p eNDKMg9sr7H4ZSnt
 REM Run the server to run the tests
 IntegrationServer -w %WORKDIR% --start-msgflows false --no-nodejs --admin-rest-api -1 --test-project TeaAvailabilityChecker_Working_Test
